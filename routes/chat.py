12b77841eb740dcc8c96bf67b8fc0ab74baca214
Naeem Sajjad <naeem9924066@gmail.com>
2026-05-21 16:05:05 +0500

    added pdf upload feature to get embeddings and context from pdf

--- routes/chat.py
+++ routes/chat.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,File,UploadFile
 from services.rag_pipeline import rag_chat
 from pydantic import BaseModel
+import shutil
+import os 
+from services.vector_store import load_pdf
 router=APIRouter()
 
 
@@ -10,3 +13,17 @@ class ChatRequest(BaseModel):
 @router.post("/chat")
 def chat(req:ChatRequest):
     return rag_chat(req.message)
+    
+@router.post("/upload-pdf")
+def upload_pdf(file: UploadFile = File(...)):
+
+    file_path = f"temp_{file.filename}"
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    load_pdf(file_path)
+
+    os.remove(file_path)
+
+    return {"message": "PDF uploaded and processed"}
